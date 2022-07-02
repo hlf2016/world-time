b9e107469979e069910ce57b7fe62c6ee71e3f64
Pannio <1821299140@qq.com>
2022-07-02 23:39:32 +0800

    feat:优化Timezone接口定义 扁平化timezone数据 中的 utc

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <script setup lang="ts">
-import timezone from 'timezones.json'
+import timezoneRaw from 'timezones.json'
 // 前端 搜索库 https://fusejs.io/
 import Fuse from 'fuse.js'
-
-// 实例化一个fuse
-const fuse = new Fuse(timezone, {
-  keys: ['utc', 'text', 'value'],
-  threshold: 0.3,
-})
 
 interface Timezone {
   name: string
   offset: number
+  isdst: boolean
 }
+
+// 扁平化 处理 timezone 数据 utc 部分
+const timezone = timezoneRaw.flatMap((i) => {
+  return i.utc.map((u) => {
+    return {
+      name: u,
+      offset: i.offset,
+      isdst: i.isdst,
+    } as Timezone
+  })
+})
+
+// 实例化一个fuse
+const fuse = new Fuse(timezone, {
+  keys: ['name'],
+  threshold: 0.3,
+})
 
 const zones = ref<Timezone[]>([])
 
@@ -27,8 +39,11 @@ const searchResults = computed(() => {
   <div>
     <input v-model="input">
     <div>
-      <div v-for="s in searchResults" :key="s.refIndex">
-        {{ s.item.text }}
+      <div v-for="s in searchResults" :key="s.refIndex" flex gap2>
+        <div font-mono w-10 text-right>
+          {{ s.item.offset }}
+        </div>
+        <div> {{ s.item.name }}</div>
       </div>
     </div>
   </div>
