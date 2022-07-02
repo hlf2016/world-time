51234e0bba733b2f38b0b6ab0f90ad57690a72f4
Pannio <1821299140@qq.com>
2022-07-02 23:09:26 +0800

    feat:引入 Fuse 前端搜素引擎

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,37 @@
 <script setup lang="ts">
+import timezone from 'timezones.json'
+// 前端 搜索库 https://fusejs.io/
+import Fuse from 'fuse.js'
 
+// 实例化一个fuse
+const fuse = new Fuse(timezone, {
+  keys: ['utc', 'text', 'value'],
+  threshold: 0.3,
+})
+
+interface Timezone {
+  name: string
+  offset: number
+}
+
+const zones = ref<Timezone[]>([])
+
+const input = ref('')
+
+const searchResults = computed(() => {
+  return fuse.search(input.value)
+})
 </script>
 
 <template>
-  <Hello name="world" />
+  <div>
+    <input v-model="input">
+    <div>
+      <div v-for="s in searchResults" :key="s.refIndex">
+        {{ s.item.text }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <style>
